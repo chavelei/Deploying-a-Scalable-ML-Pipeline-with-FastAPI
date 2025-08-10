93c6b38b737b3c7ad7983710345644a5af4f1075
Leida Chavez <leida.alvarez@yahoo.com>
2025-08-10 01:55:42 -0500

    Created three unit tests.

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,30 @@
+import os
+import pandas as pd
 import pytest
-# TODO: add necessary import
 
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
-    """
-    # add description for the first test
-    """
-    # Your code here
-    pass
+# Load dataset once for all tests
+PROJECT_PATH = "../Deploying-a-Scalable-ML-Pipeline-with-FastAPI"
+DATA_PATH = os.path.join(PROJECT_PATH, "data", "census.csv")
+data = pd.read_csv(DATA_PATH)
+MODEL_PATH = os.path.join(PROJECT_PATH, "model", "model.pkl")
+ENCODER_PATH = os.path.join(PROJECT_PATH, "model", "encoder.pkl")
 
+def test_pickles_exist():
+    """
+    Test that encoder.pkl and model.pkl files were created.
+    """
+    assert os.path.exists(MODEL_PATH), "model.pkl does not exist"
+    assert os.path.exists(ENCODER_PATH), "encoder.pkl does not exist"
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_age_range():
     """
-    # add description for the second test
+    Test that all ages are between 0 and 100.
     """
-    # Your code here
-    pass
+    assert data["age"].between(0, 100).all()
 
+def test_hours_per_week_range():
+    """
+    Test that hours worked per week are between 1 and 99.
+    """
+    assert data["hours-per-week"].between(1, 99).all()
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
-    """
-    # add description for the third test
-    """
-    # Your code here
-    pass
