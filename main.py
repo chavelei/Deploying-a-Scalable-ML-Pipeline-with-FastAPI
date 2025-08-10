9d3a42b212552a00eaaaab6ee9abbb4d105ec7a3
Leida Chavez <leida.alvarez@yahoo.com>
2025-08-10 02:17:46 -0500

    Created a RESTful API using FastAPI.

--- main.py
+++ main.py
@@ -26,21 +26,21 @@ class Data(BaseModel):
     hours_per_week: int = Field(..., example=40, alias="hours-per-week")
     native_country: str = Field(..., example="United-States", alias="native-country")
 
-path = None # TODO: enter the path for the saved encoder 
+path = "../Deploying-a-Scalable-ML-Pipeline-with-FastAPI/model/encoder.pkl"
 encoder = load_model(path)
 
-path = None # TODO: enter the path for the saved model 
+path = "../Deploying-a-Scalable-ML-Pipeline-with-FastAPI/model/model.pkl"
 model = load_model(path)
 
 # TODO: create a RESTful API using FastAPI
-app = None # your code here
+app = FastAPI()
 
 # TODO: create a GET on the root giving a welcome message
 @app.get("/")
 async def get_root():
     """ Say hello!"""
     # your code here
-    pass
+    return {"message": "Welcome to the ML model API!"}
 
 
 # TODO: create a POST on a different path that does model inference
@@ -69,6 +69,11 @@ async def post_inference(data: Data):
         # use data as data input
         # use training = False
         # do not need to pass lb as input
-    )
-    _inference = None # your code here to predict the result using data_processed
+        data,
+        categorical_features=cat_features,
+        encoder=encoder,
+        training=False,
+        label=None
+    ) 
+    _inference = inference(model, data_processed)
     return {"result": apply_label(_inference)}
